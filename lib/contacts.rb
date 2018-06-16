270a8246ef122d9b54514d3ef55c979a478d7ebf
Andrew Nikonchuk <anikonch@gmail.com>
2018-06-16 18:56:46 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -17,7 +17,10 @@ require 'pry'
 def remove_strawberry(contacts)
 
   contacts.each do |person, contact_details_hash|
-    binding.pry
+    
+    contact_details_hash.each do |attribute, data|
+      binding.pry
+    end 
   end
 
 end
